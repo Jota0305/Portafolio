f1856a847e561e1aa14edd09e48a1a40bf708725
Jose Tasilla <jozeh@hotmail.es>
2024-07-17 01:30:46 -0500

    experiment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,20 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <div className="relative">
-        <img
-          className="w-full h-[250px]"
-          src="/img/banner.jpeg"
-          alt="banner"
-        />
-        <div className="absolute inset-0 bg-black opacity-85" />
-        <div className="absolute flex items-center justify-center inset-0 gap-16">
-          <img
-            className="w-[150px] h-[170px] rounded-full"
-            src="/img/jota.jpg"
-            alt="profile"
-          />
-          <div>
-            <h1 className="text-white text-4xl">JOSE LUIS TASILLA HUARIPATA</h1>
-            <h2 className="text-white text-2xl">Frontend Developer</h2>
+      <div className="relative h-[250px] bg-[url('/img/banner.jpeg')]">
+        <div className="absolute flex justify-center inset-0 bg-black opacity-85">
+          <div className="flex items-center justify-center inset-0 gap-16">
+            <img
+              className="w-[150px] h-[170px] rounded-full"
+              src="/img/jota.jpg"
+              alt="profile"
+            />
+            <div>
+              <h1 className="text-white text-4xl">
+                JOSE LUIS TASILLA HUARIPATA
+              </h1>
+              <h2 className="text-white text-2xl">Frontend Developer</h2>
+            </div>
           </div>
         </div>
       </div>
