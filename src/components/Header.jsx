b96fed26bc89f3c872e597b1fb7d16ee1725b8e7
Jose Tasilla <jozeh@hotmail.es>
2024-07-20 01:05:47 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import banner from "../assets/img/banner.jpeg"
-import profile from "../assets/img/jota.jpg"
-import { RiGithubFill, RiLinkedinBoxFill, RiWhatsappFill, RiFacebookCircleFill } from "react-icons/ri";
+import banner from "../assets/img/banner.jpeg";
+import profile from "../assets/img/jota.jpg";
+import {
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiWhatsappFill,
+  RiMessengerFill,
+} from "react-icons/ri";
 
 function Header() {
   return (
     <div>
       <div className="relative">
-        <img
-          className="w-full h-[250px]"
-          src={banner}
-          alt="banner"
-        />
+        <img className="w-full h-[250px]" src={banner} alt="banner" />
         <div className="absolute inset-0 bg-black opacity-85" />
         <div className="absolute flex flex-col items-center justify-center inset-0 gap-6 md:flex-row md:items-center md:justify-center md:inset-0 md:gap-16">
           <img
@@ -20,20 +21,24 @@ function Header() {
             alt="profile"
           />
           <div className="text-center md:text-left">
-            <h1 className="text-2xl text-white md:text-4xl md:mb-1">JOSE LUIS TASILLA HUARIPATA</h1>
-            <h2 className="text-1xl text-white mb-1 md:text-2xl md:mb-4">Frontend Developer</h2>
-            <div className="flex gap-6 items-center justify-center md:gap-6 md:flex-row md:items-center md:justify-start">
+            <h1 className="text-2xl text-white md:text-4xl md:mb-1">
+              JOSE LUIS TASILLA HUARIPATA
+            </h1>
+            <h2 className="text-1xl text-white mb-2 md:text-2xl md:mb-4">
+              Frontend Developer
+            </h2>
+            <div className="flex gap-5 items-center justify-center md:gap-7 md:flex-row md:items-center md:justify-start">
               <a href="https://github.com/Jota0305">
-               <RiGithubFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+                <RiGithubFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full" />
               </a>
-              <a href="">
-               <RiLinkedinBoxFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+              <a href="https://www.linkedin.com/in/joseth/">
+                <RiLinkedinBoxFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full" />
               </a>
-              <a href="">
-               <RiWhatsappFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+              <a href="https://wa.me/message/6XASKC46BSZUI1">
+                <RiWhatsappFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full" />
               </a>
-              <a href="">
-               <RiFacebookCircleFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+              <a href="https://m.me/JozeLTH?hash=AbZdE6NFjlDiUbjE&source=qr_link_share">
+                <RiMessengerFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full" />
               </a>
             </div>
           </div>
