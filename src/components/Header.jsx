b490f2f06b32aa19e4f4485dd8ce59d6beced442
Jose Tasilla <jozeh@hotmail.es>
2024-07-20 00:54:05 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import banner from "../assets/img/banner.jpeg"
 import profile from "../assets/img/jota.jpg"
+import { RiGithubFill, RiLinkedinBoxFill, RiWhatsappFill, RiFacebookCircleFill } from "react-icons/ri";
 
 function Header() {
   return (
@@ -19,9 +20,22 @@ function Header() {
             alt="profile"
           />
           <div className="text-center md:text-left">
-            <h1 className="text-2xl text-white md:text-4xl">JOSE LUIS TASILLA HUARIPATA</h1>
-            <h2 className="text-1xl text-white md:text-2xl">Frontend Developer</h2>
-            
+            <h1 className="text-2xl text-white md:text-4xl md:mb-1">JOSE LUIS TASILLA HUARIPATA</h1>
+            <h2 className="text-1xl text-white mb-1 md:text-2xl md:mb-4">Frontend Developer</h2>
+            <div className="flex gap-6 items-center justify-center md:gap-6 md:flex-row md:items-center md:justify-start">
+              <a href="https://github.com/Jota0305">
+               <RiGithubFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+              </a>
+              <a href="">
+               <RiLinkedinBoxFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+              </a>
+              <a href="">
+               <RiWhatsappFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+              </a>
+              <a href="">
+               <RiFacebookCircleFill className="w-5 h-5 md:w-8 md:h-8 bg-white rounded-full"/>
+              </a>
+            </div>
           </div>
         </div>
       </div>
